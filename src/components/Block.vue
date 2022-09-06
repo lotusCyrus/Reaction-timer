9c137df7da91568bef33e620c4347210fc58f46c
lotusCyrus <johnsonsolotu@gmail.com>
2022-09-06 13:37:27 +0100

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,59 @@
+<template>
+    
+    <div class="block" v-if="showblock">
+        click me
+    </div>
+    
+</template>
+<script>
+  export default{
+      props:['delay'],
+      data(){
+          return{
+          showblock:false,
+          timer:null,
+          reactionTime:0
+          }
+      },
+mounted()
+  {
+    console.log("components mounted")
+    setTimeout(()=>{
+        this.showblock=true
+        this.startTimer()
+    }, this.delay)
+  },
+
+updated()
+{
+    console.log("component updated")
+},
+ummounted(){
+    console.log("unmounted")
+},
+
+methods:{
+    startTimer(){
+        this.timer=setInterval(()=>{
+        
+        },this.delay)
+    },
+    stopTimer(){
+        
+    }
+}
+  }  
+  
+</script>
+<style>
+   .block{
+       width:200px;
+       border-radius:20px;
+       background: #0faf87;
+       color:white;
+       text-align: center;
+       padding:100px 0;
+       margin:40px auto;
+   } 
+    
+</style>
